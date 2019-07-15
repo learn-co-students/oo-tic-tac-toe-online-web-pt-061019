1f79d24e09a69cd3c124e8b3c15ebb36baca2627
Lantz Warrick <betalantz@gmail.com>
2019-07-15 01:05:07 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,4 +34,74 @@ class TicTacToe
     @board[index] = move
   end
 
+  def position_taken?(index)
+    @board[index] != " " ? true : false
+    # !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0, 8) && !self.position_taken?(index)
+    # @board[index] == " " ? true : false && index == (/[0-8]/)
+    # !self.position_taken(index)
+  end
+
+  def turn_count
+    @board.count {|pos| pos == "X" || pos == "O"}
+  end
+
+  def current_player
+    self.turn_count % 2 != 0 ? "O" : "X"
+  end
+
+  def turn
+    puts "Where would you like to move?"
+    input = gets.strip
+    index = self.input_to_index(input)
+    if self.valid_move?(index)
+      # player = self.current_player
+      self.move(index, self.current_player)
+      self.display_board
+    else
+      self.turn
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect {|win_combo| win_combo.all?{|pos| @board[pos]=="X"} || win_combo.all?{|pos| @board[pos]=="O"}}
+  end
+
+  def full?
+    self.turn_count == 9
+  end
+
+  def draw?
+    self.full? && !self.won?
+  end
+
+  def over?
+    self.draw? || self.won?
+  end
+
+  def winner
+    # if !won?
+    #   nil
+    # elsif won?.all?{|pos| @board[pos]=="X"}
+    #   "X"
+    # # elsif won?.all?{|pos| @board[pos]=="O"}
+    # #   "O"
+    # else
+    #   "O"
+    # end
+    return nil unless won?
+    @board[self.won?[0]]
+  end
+
+  def play
+    until over?
+      turn
+    end
+    puts "Congratulations #{self.winner}!" if self.won?
+    puts "Cat's Game!" if self.draw?
+  end
+
 end
